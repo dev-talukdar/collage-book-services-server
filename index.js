0e9b109076bea25092de5585273372dd45336bc6
dev-talukdar <dev.talukdaar@gmail.com>
2023-07-24 21:50:23 +0600

    env file created

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors'); 
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-//i'm checking
+ 
 
 app.get('/', (req, res) => {
     res.send('server is running')
